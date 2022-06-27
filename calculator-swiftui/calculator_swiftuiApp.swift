c43119ad9a5f82988f40000588e3dc672397b724
Rick Brown <ricky.brown.00@gmail.com>
2022-06-27 10:45:08 +0100

    Initial Commit

---
+++ calculator-swiftui/calculator_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_swiftuiApp.swift
+//  calculator-swiftui
+//
+//  Created by Rick Brown on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct calculator_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
